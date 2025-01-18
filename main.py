aa87b5f77eb065237228395b9b6ba2c155426191
Noblezgg <zachnoble235@gmail.com>
2025-01-17 19:46:19 -0800

    Initial source commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
+
